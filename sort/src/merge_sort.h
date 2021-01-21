775e5650842f7c0d8f498eadaae5530a42015ee2
Andrea Carlo Marini <andrea.marini@cern.ch>
2021-01-21 14:54:14 +0100

    merge sort

---
+++ sort/src/merge_sort.h
@@ -0,0 +1,56 @@
+
+
+// Function to merge the two haves arr[l..m]
+// and arr[m+1..r] of array arr[] 
+template<typename T>
+void merge(T in1[], T in2[], T out[], int InSize1, int InSize2, int OutSize)
+{
+    int l=0,r=0; // position in the 1/2 array
+    for( int i=0;i<OutSize;++i){
+    #pragma HLS pipeline II=1
+        if (l>=InSize1 && r >=InSize2) continue; // throw error
+        if (l>=InSize1) out[i] = in2[r];
+        if (r>=InSize2) out[i] = in1[l];
+        if (in1[l] < in2[r]) { 
+            out[i] = in2[r]; ++r;
+        } else {  // >=
+            out[i] = in1[l];
+            ++l;
+        }
+    }
+}
+ 
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted 
+template <typename T>
+void mergeSort(T in[], T out[], int InSize, int OutSize) // InSize = OutSize
+{
+    for(int size=1;size < 2*InSize ; size*=2 ) // miss last
+    {
+    //#pragma HLS pipeline II=1
+        // 2 chunks of size are being merged
+        for(int l=0;l<InSize;l+=2*size){
+            #pragma HLS unroll
+            int leftsize=(l+size<=InSize)?size:(InSize -l);
+            int rightsize=(l+leftsize+size<=InSize)?size:(Insize-l-leftsize);
+            merge(in+l, in+l+leftsize, out+l, leftsize, rightsize, leftsize+rightsize); 
+        }
+
+        // copy back to in for next iteration
+        if(size <= InSize){
+            for(int i=0;i<InSize;++i){
+                #pragma HLS unroll
+                in[i] = out[i];
+            }
+        }
+    }
+
+}
+
+//    template<typename T>
+//void sort_and_crop_ref(unsigned int nIn, unsigned int nOut, T in[], T out[]) 
+//{  // just an interface
+//    mergeSort(in,0, nIn,out, nOut,0); 
+//}
+ 
