d28b66503bd053cc9c47a51a091ebc1dacdce34d
Andrea Carlo Marini <andrea.marini@cern.ch>
2021-01-21 15:32:55 +0100

    merge sort

--- sort/src/merge_sort.h
+++ sort/src/merge_sort.h
@@ -1,5 +1,3 @@
-
-
 // Function to merge the two haves arr[l..m]
 // and arr[m+1..r] of array arr[] 
 template<typename T>
@@ -7,15 +5,17 @@ void merge(T in1[], T in2[], T out[], int InSize1, int InSize2, int OutSize)
 {
     int l=0,r=0; // position in the 1/2 array
     for( int i=0;i<OutSize;++i){
-    #pragma HLS pipeline II=1
-        if (l>=InSize1 && r >=InSize2) continue; // throw error
-        if (l>=InSize1) out[i] = in2[r];
-        if (r>=InSize2) out[i] = in1[l];
+        #pragma HLS pipeline II=1
+        if (l>=InSize1 && r >=InSize2) {
+            //std::cout<<"[ERROR]"<<__FUNCTION__<<std::endl;
+            continue; // throw error
+        }
+        if (l>=InSize1) {out[i] = in2[r++]; continue;}
+        if (r>=InSize2) {out[i] = in1[l++]; continue;}
         if (in1[l] < in2[r]) { 
-            out[i] = in2[r]; ++r;
+            out[i] = in2[r++]; 
         } else {  // >=
-            out[i] = in1[l];
-            ++l;
+            out[i] = in1[l++];
         }
     }
 }
@@ -26,14 +26,14 @@ void merge(T in1[], T in2[], T out[], int InSize1, int InSize2, int OutSize)
 template <typename T>
 void mergeSort(T in[], T out[], int InSize, int OutSize) // InSize = OutSize
 {
-    for(int size=1;size < 2*InSize ; size*=2 ) // miss last
+    for(int size=1;size < /*2*/InSize ; size*=2 ) // miss last
     {
     //#pragma HLS pipeline II=1
         // 2 chunks of size are being merged
         for(int l=0;l<InSize;l+=2*size){
             #pragma HLS unroll
             int leftsize=(l+size<=InSize)?size:(InSize -l);
-            int rightsize=(l+leftsize+size<=InSize)?size:(Insize-l-leftsize);
+            int rightsize=(l+leftsize+size<=InSize)?size:(InSize-l-leftsize);
             merge(in+l, in+l+leftsize, out+l, leftsize, rightsize, leftsize+rightsize); 
         }
 
@@ -48,9 +48,14 @@ void mergeSort(T in[], T out[], int InSize, int OutSize) // InSize = OutSize
 
 }
 
-//    template<typename T>
-//void sort_and_crop_ref(unsigned int nIn, unsigned int nOut, T in[], T out[]) 
-//{  // just an interface
-//    mergeSort(in,0, nIn,out, nOut,0); 
-//}
+    template<typename T>
+void sort_and_crop_ref(unsigned int nIn, unsigned int nOut, T in[], T out[]) 
+{  // just an interface
+    T outTmp[nIn];
+    mergeSort(in,outTmp,nIn,nIn); 
+    for(int i=0;i<nOut;++i){
+        #pragma HLS unroll
+        out[i] = outTmp[i];
+    }
+}
  
